dc1aa119f764378782e71ea5d55cf71c3b4c3960
Ruslan_Yakovlev <r.yakovlev@takeoff-staff.ru>
2021-02-03 11:24:22 +0500

    fc

---
+++ .yalc/reactsandbox/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <button onClick={() => { console.log('hey'); }} >Hey</button>
+  );
+}
+
+export default App;
---
+++ .yalc/test/test.js
@@ -0,0 +1,11 @@
+export class Consoler {
+    static hey() {
+        console.log('hey');
+    }
+
+    static hello() {
+        console.log('hello');
+    }
+}
+
+export default Consoler;
